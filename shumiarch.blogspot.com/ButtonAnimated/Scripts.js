cea6b62a2a852876d0fa76bba8522b61ba602bbf
Setya Himawan <115913091+Setya-Himawan@users.noreply.github.com>
2024-07-31 15:18:03 +0700

    Update Scripts.js

--- shumiarch.blogspot.com/ButtonAnimated/Scripts.js
+++ shumiarch.blogspot.com/ButtonAnimated/Scripts.js
@@ -89,7 +89,7 @@ if (document.body.animate) {
 function applyWrapperStyles() {
     const wrapper = document.querySelector('.wrapper');
 
-    if (window.matchMedia("(max-width: 760px)").matches) {
+    if (window.matchMedia("(max-width: 768px)").matches) {
         // Jika lebar layar kurang dari atau sama dengan 411px
         wrapper.style.display = 'flex';
         wrapper.style.flexDirection = 'column';
